9678f6b6367dae1bd7a84c6fb01c12b53e01e024
Đặng Quốc Huy <85658305+danieldev23@users.noreply.github.com>
2024-06-04 23:04:21 +0700

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require("cors");
 const db = require('./config/db');
 const port = 3000;
 const app = express();
@@ -7,6 +8,9 @@ const route = require('./routes');
 const bodyParser = require('body-parser');
 
 db.connect();
+app.use(cors({
+  origin: 'http://localhost:5173' // Cho phép yêu cầu từ nguồn gốc này
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 route(app)
